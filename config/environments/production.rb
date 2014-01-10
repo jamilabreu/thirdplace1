dde17f9b7e7d006fa4f27d320094d38992eb69cf
Jamil Abreu <abreu.jamil@gmail.com>
2014-01-09 20:50:11 -0500

    heroku prep

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( active_admin.js imagesloaded.min.js infinitescroll.min.js packery.min.js select2.min.js users-edit.js users.js active_admin.css select2.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
